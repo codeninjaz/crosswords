b6ba08b5d75461b92ab3f7102aeb39fe26e7f434
shoobah <shoobah@gmail.com>
2018-02-21 21:10:43 +0100

    New gitignore in dist

--- dist/createWordFile.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const grepthem_1 = require("./getwords/grepthem");
-const gw = new grepthem_1.GrepWords();
-gw.writeWordsToFile();
-//# sourceMappingURL=createWordFile.js.map
